c830a90613077cf74e54193f8c3b050d80c3e66e
Wolfgang Steiner <git@steinerwolfgang.de>
2017-12-18 16:27:21 +0100

    SNMP Beispiel hat gefehlt

--- Tools/copy.cmd
+++ Tools/copy.cmd
@@ -4,5 +4,5 @@ robocopy "C:\ProgramData\Schneider Electric\Vijeo Citect 7.50\User\GUT30_P3" "D:
 robocopy "C:\ProgramData\Schneider Electric\Vijeo Citect 7.50\User\GUT30_Bild19HD" "D:\Schneider Application\GUT30\GUT30_Bild19HD" /MIR /DST /MT:8 /FFT /R:5 /ZB /W:1 /TEE /Log+:"D:\Schneider Application\logs\git_Bild19HD_sicherung.txt
 
 robocopy "C:\ProgramData\Schneider Electric\Vijeo Citect 7.50\User\GUT30_Demo_Unity_OFS" "D:\Schneider Application\GUT30\GUT30_Demo_Unity_OFS" /MIR /DST /MT:8 /FFT /R:5 /ZB /W:1 /TEE /Log+:"D:\Schneider Application\logs\git_Demo_Unity_OFS_sicherung.txt
-robocopy "C:\ProgramData\Schneider Electric\Vijeo Citect 7.50\User\GUT30_Demo_Unity_OFS_P4" "D:\Schneider Application\GUT30\GUT30_Demo_Unity_OFS_P4" /MIR /DST /MT:8 /FFT /R:5 /ZB /W:1 /TEE /Log+:"D:\Schneider Application\logs\git_Demo_Unity_OFS_sicherung.txt
+robocopy "C:\ProgramData\Schneider Electric\Vijeo Citect 7.50\User\GUT30_SNMP" "D:\Schneider Application\GUT30\GUT30_SNMP" /MIR /DST /MT:8 /FFT /R:5 /ZB /W:1 /TEE /Log+:"D:\Schneider Application\logs\git_Demo_Unity_OFS_sicherung.txt
 
